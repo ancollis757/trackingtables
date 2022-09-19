7ce5a294e88377c191a1a89191da7f29c2e0f584
branches1 <105700994+branches1@users.noreply.github.com>
2022-09-19 19:47:31 +0100

    basic uploader
    not working; date conflict error.

---
+++ InsertData_neckinj.sql
@@ -0,0 +1,19 @@
+INSERT INTO neckinj (
+	LogDate,
+	RightPain,
+	RightNumb,
+	LeftPain,
+	LeftNumb,
+	LeftElbowPain,
+	LeftElbowNumb,
+	Notes,
+	ExternalityAdjustment
+)
+VALUES
+-- Take values from the uploader.
+-- No data to be uploaded into this query as saved on GitHub.
+-- Data to be saved into the table only, not onto GitHub.
+-- Format as below.
+(01-01-1900, 0, 0, 0, 0, 0, 0,'Some stuff', 0)
+
+--Failing on date is in the wrong format. Watch the lectures on dates again.
