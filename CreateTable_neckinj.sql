5c83db9692f07f78bd59b1b3ba7ec060ee14819f
branches1 <105700994+branches1@users.noreply.github.com>
2022-09-19 19:20:04 +0100

    Create CreateTable_neckinj.sql

---
+++ CreateTable_neckinj.sql
@@ -0,0 +1,11 @@
+CREATE TABLE NeckInj (
+	LogDate date,
+	RightPain smallint,
+	RightNumb smallint,
+	LeftPain smallint,
+	LeftNumb smallint,
+	LeftElbowPain smallint,
+	LeftElbowNumb smallint,
+	Notes varchar(100),
+	ExternalityAdjustment smallint
+)
