978faf4dfc8e70ac097d958d49db119102ef4f87
branches1 <109507255+ancollis757@users.noreply.github.com>
2022-09-23 18:29:33 +0100

    SQL Upload Setup

--- InsertData_neckinj.sql
+++ InsertData_neckinj.sql
@@ -14,6 +14,12 @@ VALUES
 -- No data to be uploaded into this query as saved on GitHub.
 -- Data to be saved into the table only, not onto GitHub.
 -- Format as below.
+
+--INSTRUCTIONS
+--1. Add the data from the template.
+--2. 
+
+
 (1900-01-01, 0, 0, 0, 0, 0, 0,'Some stuff', 0)
 
 --Failing on date is in the wrong format. Watch the lectures on dates again.
