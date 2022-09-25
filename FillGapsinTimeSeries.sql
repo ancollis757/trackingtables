53947bd0d70e791f8e29e979d2f4bcb86fe09cf9
branches1 <109507255+ancollis757@users.noreply.github.com>
2022-09-25 14:05:24 +0100

    Time Series Gap Filler First Attempt
    Complete but untested.

---
+++ FillGapsinTimeSeries.sql
@@ -0,0 +1,22 @@
+DECLARE @START_DATE DATE
+
+SET @START_DATE = '2020-03-10' --Hard coded start date.
+
+SELECT
+	DATEADD(DAY, I, @START_DATE) AS LogDate
+	,RightPain
+	,RightNumb
+	,LeftPain
+	,LeftNumb
+	,LeftBelowPain
+	,LeftElbowNumb
+	,Notes
+	,ExternalityAdjustment
+FROM
+	dbo.GetNums2(870) --Returns 870 rows, the length of the total number of days if no gaps.
+LEFT OUTER JOIN
+	dbo.NeckInj T
+ON
+	DATEADD(DAY, I, @START_DATE) = T.LogDate
+ORDER BY
+	LogDate
