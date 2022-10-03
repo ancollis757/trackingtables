97c78e40ce88b2ec1e8aa28921534126488938f3
branches1 <109507255+ancollis757@users.noreply.github.com>
2022-10-03 20:47:16 +0100

    Update NIReporter.py
    Starting adding chart functionality.

--- NIReporter.py
+++ NIReporter.py
@@ -1,6 +1,7 @@
 import pyodbc
 import pandas as pd
 from datetime import datetime
+from matplotlib import pyplot as plt
 
 REPORT_COLUMNS = [
     'LogStart',
@@ -35,8 +36,35 @@ fd.close()
 all_queries_in_file = sqlFile.split(';')  # all SQL commands (split on ';')
 query = all_queries_in_file[0]  # Extract the first query.
 
-# Pull data from DB using the query, take the mean and then put into a pandas DF.
+# Extract the data from the table.
 neckinj_Table = pd.read_sql(query, cnxn)
+
+# Produce plot of total against time.
+date_range_x_axis = neckinj_Table['LogDate'].tolist()
+neckinj_Table['NITotal'] = neckinj_Table['LeftPain'] + neckinj_Table['LeftNumb'] + neckinj_Table['RightPain'] + neckinj_Table['RightNumb']
+date_range_y_axis = neckinj_Table['NITotal'].tolist()
+ni_total_plot = plt.plot(date_range_x_axis, date_range_y_axis, label='Muhline')
+
+x = [3,4,5]
+y = [6,7,8]
+
+plt.plot
+
+print(type(plt.plot(date_range_x_axis, date_range_y_axis, label='Muhline')))
+print(type(ni_total_plot))
+
+# naming the x axis
+ni_total_plot.xlabel('Log Date')
+# naming the y axis
+ni_total_plot.ylabel('Total NI')
+# giving a title to my graph
+ni_total_plot.title('NI by Time')
+# function to show the plot
+ni_total_plot.show()
+ni_total_plot.ylim(0, 8)
+ni_total_plot.xlim(0, 8)
+
+
 unformatted_results = neckinj_Table.groupby(['PeriodName']).mean()
 unformatted_results = pd.DataFrame(unformatted_results)  # Making a DF from the group-by object
 
@@ -52,7 +80,7 @@ log_End = pd.DataFrame(log_End)
 log_End = pd.DataFrame(log_End['LogDate'])
 log_End.rename(columns={"LogDate": "LogEnd"}, inplace=True)
 
-# Produce the completed report.
+# Produce the summary.
 ni_date_ranges = log_End.join(log_Start)  # Join axis not specified; defaults to index.
 ni_report = ni_date_ranges.join(unformatted_results)
 ni_report.sort_values(['LogStart'], axis=0, inplace=True)
@@ -64,3 +92,4 @@ today = datetime.today().strftime('%Y-%m-%d')
 output_filename = FILENAME_STEM + today + '.xlsx'
 output_full = OUTPUT_LOCATION + output_filename
 ni_report.to_excel(output_full, float_format='%.2f')
+
