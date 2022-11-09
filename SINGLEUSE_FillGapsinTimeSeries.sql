0b6ece2fc12a3ea011c1028b9d95eb0f948de140
branches1 <109507255+ancollis757@users.noreply.github.com>
2022-11-09 08:26:15 +0000

    Minor mistakes fixed in uploaded

--- SINGLEUSE_FillGapsinTimeSeries.sql
+++ SINGLEUSE_FillGapsinTimeSeries.sql
@@ -12,13 +12,14 @@ SELECT
 	,LeftElbowNumb
 	,Notes
 	,ExternalityAdjustment
+	,PeriodName
 
 INTO dbo.NeckInj_GapFilled --Create a new table from the select. Comment out if just want to see result.
 
 FROM
-	dbo.GetNums2(923) --Returns 923 rows, the length of the total number of days if no gaps.
+	dbo.GetNums2(974) --Returns 974 rows, the length of the total number of days between end of last uploaded date range and start date.
 LEFT OUTER JOIN
-	dbo.NeckInj T
+	dbo.NeckInj_wPeriod T
 ON
 	DATEADD(DAY, I, @START_DATE) = T.LogDate
 ORDER BY
