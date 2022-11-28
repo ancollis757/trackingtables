0cbf7001a355d381ba857241b8929af6453d473d
branches1 <109507255+ancollis757@users.noreply.github.com>
2022-11-28 08:12:15 +0000

    SQL Connection Established

--- SelectfromNeckinj_wPeriod.sql
+++ SelectfromNeckinj_wPeriod.sql
@@ -1,5 +1,5 @@
 /****** Script for SelectTopNRows command from SSMS  ******/
-SELECT TOP (1000) [LogDate]
+SELECT [LogDate]
       ,[LeftPain]
       ,[LeftNumb]
 	  ,[RightPain]
--- Tracking-Import-Export.py
+++ Tracking-Import-Export.py
@@ -7,32 +7,36 @@ import os
 
 ############  GLOBAL VARIABLES  ###########
 
-TRACKER_NAMES = ['', '']
-TRACKER_TABLE_NAMES = ['', '']
-TRACKER_SPREADSHEET_NAMES = ['', '']
-SPREADSHEETS_DIRECTORY = r'blah'
-DATABASE_NAME = 'tracking'
-SERVER = 'DESKTOP-IH85PI5'
+TRACKER_NAMES = ['NeckInj', 'WTT']
+TRACKER_TABLE_NAMES = ['NeckInj_wPeriod', 'WTT']
+TRACKER_SPREADSHEET_NAMES = ['NeckInj_Uploader.xlsx', 'WTT_Uploader.xlsx']
+SPREADSHEETS_DIRECTORY = r'C:\Users\nicko\Documents\GitHub\trackingtables'
+SQL_EXTRACT_QUERIES = ['SelectfromNeckinj_wPeriod.sql','']
+
+# Issue of linking to variables not yet fixed. See SQL import method for hard-coding of server and database names.
+# SERVER_STRING = 'Server=DESKTOP-IH85PI5;'  # Cannot insert variable in cnxn statement so global whole string.
+# DATABASE_CNXN_STRING = 'Database=tracking;'  # Cannot insert variable in cnxn statement so global whole string.
 
 #################  CLASSES  ###############
 
 class Tracker:
-    def __init__(self, tracker_name, tracker_table_name, tracker_spreadsheet_name):
+    def __init__(self, tracker_name, tracker_table_name, tracker_spreadsheet_name, sql_extract_query):
+        print("New tracker instance being creating...")
         self.tracker_name = tracker_name
         self.tracker_table_name = tracker_table_name
         self.tracker_spreadsheet_name = tracker_spreadsheet_name
-
-        print("New tracker instance being creating...")
+        self.sql_extract_query = sql_extract_query
 
     def import_sql(self):
+        print(f"Importing table for {self.tracker_name}...")
         # Make connection to the on-premises database.
         cnxn = pyodbc.connect('Driver={SQL Server};'
                               'Server=DESKTOP-IH85PI5;'
-                              'Database=DATABASE_NAME;' 
+                              'Database=tracking;'
                               'Trusted_Connection=yes;')
         cursor = cnxn.cursor()
         # Get out the SQL query
-        fd = open(self.tracker_table_name, 'r')
+        fd = open(self.sql_extract_query, 'r')
         sqlFile = fd.read()
         fd.close()
         all_queries_in_file = sqlFile.split(';')  # all SQL commands (split on ';')
@@ -41,6 +45,9 @@ class Tracker:
         # Extract the data from the table.
         self.unprocessed_sql_data = pd.read_sql(query, cnxn)
 
+
 #################  MAIN RUN  ###############
 
 os.chdir(SPREADSHEETS_DIRECTORY)  # Set current working directory to location of data spreadsheets.
+tracker_1 = Tracker(TRACKER_NAMES[0], TRACKER_TABLE_NAMES[0], TRACKER_SPREADSHEET_NAMES[0], SQL_EXTRACT_QUERIES[0])
+tracker_1.import_sql()
