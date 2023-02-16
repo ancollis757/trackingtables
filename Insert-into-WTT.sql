1accc1ddf58d13be765902fc7bd3fc80b34fc59f
branches1 <109507255+ancollis757@users.noreply.github.com>
2023-02-16 07:56:38 +0000

    Insert file created
    Still working on switch to csv.

---
+++ Insert-into-WTT.sql
@@ -0,0 +1,7 @@
+INSERT INTO dbo.WTT (
+	LogDate,
+	LIT_Minutes,
+	OW_Minutes,
+		Notes
+)
+VALUES
--- Tracking-Import-Export.py
+++ Tracking-Import-Export.py
@@ -7,12 +7,12 @@ import os
 
 ############  GLOBAL VARIABLES  ###########
 
-TRACKER_NAMES = ['NeckInj', 'WTT']
-TRACKER_TABLE_NAMES = ['NeckInj_wPeriod', 'WTT']
+TRACKER_NAMES = ['WTT', 'NeckInj']
+TRACKER_TABLE_NAMES = ['WTT', 'NeckInj_wPeriod']
 RAW_DATA_FILES = ['WTT_csv_upload.csv', 'NINJ_csv_upload.csv']
 SPREADSHEETS_DIRECTORY = r'C:\Users\nicko\Documents\GitHub\trackingtables'
-SQL_EXTRACT_QUERIES = ['SelectfromNeckinj_wPeriod.sql', '']
-
+SQL_SELECT_QUERIES = ['SelectfromWTT.sql', 'SelectfromNeckinj_wPeriod.sql']
+SQL_INSERT_QUERIES = ['Insert-into-WTT.sql', 'Insert-into-Neckinj_wPeriod.sql']
 
 # Issue of linking to variables not yet fixed. See SQL import method for hard-coding of server and database names.
 # SERVER_STRING = 'Server=DESKTOP-IH85PI5;'  # Cannot insert variable in cnxn statement so global whole string.
@@ -21,11 +21,11 @@ SQL_EXTRACT_QUERIES = ['SelectfromNeckinj_wPeriod.sql', '']
 #################  CLASSES  ###############
 
 class Tracker:
-    def __init__(self, tracker_name, tracker_table_name, tracker_spreadsheet_name, sql_extract_query):
+    def __init__(self, tracker_name, tracker_table_name, tracker_rawdatafile_name, sql_extract_query):
         print("New tracker instance being creating...")
         self.tracker_name = tracker_name
         self.tracker_table_name = tracker_table_name
-        self.tracker_spreadsheet_name = tracker_spreadsheet_name
+        self.tracker_rawdatafile_name = tracker_rawdatafile_name
         self.sql_extract_query = sql_extract_query
 
     def import_sql(self):
@@ -48,13 +48,15 @@ class Tracker:
         print(f"Here is the gapped sql data for {self.tracker_name}")
 
     def import_live_data(self):
-        print(f"Importing live data for {self.tracker_name}...")
-        self.raw_live = pd.read_csv(self.tracker_spreadsheet_name)
-        print(f"Here is the raw spreadsheet data for {self.raw_live}")
+        print(f"Importing live data for {self.tracker_name}...\n\n")
+        self.raw_live = pd.read_csv(self.tracker_rawdatafile_name)
+        print(f"Here is the raw spreadsheet data for {self.raw_live}\n\n")
+        print(f"Here are the column names:\n\n {self.raw_live.columns}")
 
 #################  MAIN RUN  ###############
 
 os.chdir(SPREADSHEETS_DIRECTORY)  # Set current working directory to location of data spreadsheets.
-tracker_1 = Tracker(TRACKER_NAMES[0], TRACKER_TABLE_NAMES[0], RAW_DATA_FILES[0], SQL_EXTRACT_QUERIES[0])
+tracker_1 = Tracker(TRACKER_NAMES[0], TRACKER_TABLE_NAMES[0], RAW_DATA_FILES[0], SQL_SELECT_QUERIES[0])
 tracker_1.import_sql()
 tracker_1.import_live_data()
+
